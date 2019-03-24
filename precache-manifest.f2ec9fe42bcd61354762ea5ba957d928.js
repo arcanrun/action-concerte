6512e5cd61d750d9c47379d2904fb5b50eb9de91
arcanrun <arcan.run@gmail.com>
2019-03-24 11:08:40 +0300

    Updates

---
+++ precache-manifest.f2ec9fe42bcd61354762ea5ba957d928.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d942223eccba9af2e62e0f955671d29",
+    "url": "/redux-picSaver/static/media/Lobster.2d942223.ttf"
+  },
+  {
+    "revision": "19e6011fbf54b2f301d8",
+    "url": "/redux-picSaver/static/js/runtime~main.19e6011f.js"
+  },
+  {
+    "revision": "b47f0224bb7106badb9c",
+    "url": "/redux-picSaver/static/js/main.b47f0224.chunk.js"
+  },
+  {
+    "revision": "e18dcaa03664be883b60",
+    "url": "/redux-picSaver/static/js/1.e18dcaa0.chunk.js"
+  },
+  {
+    "revision": "b47f0224bb7106badb9c",
+    "url": "/redux-picSaver/static/css/main.6ff36e5a.chunk.css"
+  },
+  {
+    "revision": "e18dcaa03664be883b60",
+    "url": "/redux-picSaver/static/css/1.7144c2d3.chunk.css"
+  },
+  {
+    "revision": "076e4a73c5802e92e7f32a88c239b3b7",
+    "url": "/redux-picSaver/index.html"
+  }
+];
