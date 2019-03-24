45f14994121938695af92c817f489e67bc87fe70
arcanrun <arcan.run@gmail.com>
2019-03-24 12:41:43 +0300

    Updates

---
+++ precache-manifest.6a609f73b0fa89e05024afe6c3c1dc8e.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed7ba32d124842f2d21d89022529c721",
+    "url": "/action-concerte/static/media/fire-active.ed7ba32d.svg"
+  },
+  {
+    "revision": "95dd2d8dcd88bd0927c7",
+    "url": "/action-concerte/static/css/main.310c7f3c.chunk.css"
+  },
+  {
+    "revision": "fb6a511f6f53cac05c8e718f49131340",
+    "url": "/action-concerte/static/media/bg-gifts.fb6a511f.jpg"
+  },
+  {
+    "revision": "ae06c636763d378401e4",
+    "url": "/action-concerte/static/js/1.ae06c636.chunk.js"
+  },
+  {
+    "revision": "c4169e70ea245ddaccce",
+    "url": "/action-concerte/static/js/runtime~main.c4169e70.js"
+  },
+  {
+    "revision": "6c06f3e72f063fab5b5eaa430aa93cec",
+    "url": "/action-concerte/static/media/heart.6c06f3e7.svg"
+  },
+  {
+    "revision": "c4cfb5ca54dcf9fa04a387f476d4e193",
+    "url": "/action-concerte/static/media/heart_red.c4cfb5ca.svg"
+  },
+  {
+    "revision": "14c32a6dd011b2992236d9be20bf844a",
+    "url": "/action-concerte/static/media/menu.14c32a6d.svg"
+  },
+  {
+    "revision": "2eb131d845bdc5a1803fbf6d21377cf8",
+    "url": "/action-concerte/static/media/fire.2eb131d8.svg"
+  },
+  {
+    "revision": "95dd2d8dcd88bd0927c7",
+    "url": "/action-concerte/static/js/main.95dd2d8d.chunk.js"
+  },
+  {
+    "revision": "d98ec07c06f535d46cdfdce2e2da19fb",
+    "url": "/action-concerte/static/media/FuturaBookC.d98ec07c.ttf"
+  },
+  {
+    "revision": "b79b743427b7ecfcffe86bc8bc0974fd",
+    "url": "/action-concerte/static/media/FuturaDemiC.b79b7434.ttf"
+  },
+  {
+    "revision": "3f4b5cece695801d8644ffe63186e00f",
+    "url": "/action-concerte/static/media/FuturaLightC.3f4b5cec.ttf"
+  },
+  {
+    "revision": "249863ea055b74965b81e885009f9ac6",
+    "url": "/action-concerte/static/media/FuturaMediumC.249863ea.ttf"
+  },
+  {
+    "revision": "2d942223eccba9af2e62e0f955671d29",
+    "url": "/action-concerte/static/media/Lobster.2d942223.ttf"
+  },
+  {
+    "revision": "de5f308611480502173af83f249dc899",
+    "url": "/action-concerte/static/media/bg-gifts-3.de5f3086.jpg"
+  },
+  {
+    "revision": "f02582ec0b844cfad99589aed1111e75",
+    "url": "/action-concerte/static/media/bg-gifts-2.f02582ec.jpg"
+  },
+  {
+    "revision": "ae06c636763d378401e4",
+    "url": "/action-concerte/static/css/1.68c26567.chunk.css"
+  },
+  {
+    "revision": "e2254bb91e2304ec113c3aef04fa6430",
+    "url": "/action-concerte/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/redux-picSaver/precache-manifest.7ac03e59fd9082fe19f877296b31773b.js"
+  "/action-concerte/precache-manifest.6a609f73b0fa89e05024afe6c3c1dc8e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/redux-picSaver/index.html", {
+workbox.routing.registerNavigationRoute("/action-concerte/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
